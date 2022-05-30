28bb3d10fd53ee3b9ca5f7c307c74479a29a58c6
Azad <Azad@Ajinkya>
2022-05-30 18:38:25 +0530

    Commited addition and subtraction

--- src/demo1/Addition.java
+++ src/demo1/Addition.java
@@ -1,13 +1,29 @@
 package demo1;
 
-public class Addition {
-
-	public static void main(String[] args) {
-		
-		int a=10;
-		int b=20;
+public class Addition 
+{
+	static int a=10;
+	static int b=20;
+	public static void Add()
+	{
 		int c=a+b;
-		System.out.println(c);
+		System.out.println(c);	
+	}
+	
+	public static void sub()
+	{
+		int d=b-a;
+		System.out.println(d);
+	}
+	
+
+	public static void main(String[] args)
+	{
+		
+		Addition.Add();
+		Addition.sub();
+		
+		
 	}
 
 }
